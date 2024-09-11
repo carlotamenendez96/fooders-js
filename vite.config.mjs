2dc2c4e9d2c445f6190d85c33ab43c97557e990f
carlota-home <carlotamenendez96@hotmail.es>
2024-09-11 13:52:45 +0200

    update vite.config.mjs and static.yml

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fooders-js/', // Añade esta línea para configurar el base
   plugins: [
     VueRouter(),
     Vue({
